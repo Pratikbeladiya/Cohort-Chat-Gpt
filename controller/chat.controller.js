4363bade03cba0cf9cb996e92296068956de3180
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-09-10 08:35:39 +0530

    feat: Implement Chat Controller #10

---
+++ controller/chat.controller.js
@@ -0,0 +1,20 @@
+const chatModel = require("../model/chat.model");
+
+async function createChat(req,res){
+    const{title}=req.body;
+    const user =req.user;
+
+   const chat = await chatModel.create({
+    user:user._id,
+    title
+   });
+   
+   res.status(201).json({
+    message:"Chat created successfully",
+    chat
+   });
+}
+
+module.exports ={
+    createChat
+}
