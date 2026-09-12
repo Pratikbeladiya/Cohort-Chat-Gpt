850caa7ecd4b7775c2bf403617510e11b9c5912d
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-09-12 13:02:20 +0530

    add thing to get clarity of what i implement

--- route/chat.route.js
+++ route/chat.route.js
@@ -3,7 +3,7 @@ const route = express .Router();
 const chatController = require("../controller/chat.controller");
 const { authUser } = require("../middleware/auth.middleware");
 
-
+//post of chat route
 route.post("/chat", authUser, chatController.createChat);
 
 
