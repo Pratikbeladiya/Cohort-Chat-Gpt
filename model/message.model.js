fb5c4be56bae77f651f4484814d2444290b5b70e
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-09-12 19:03:48 +0530

    chnages in the message model

--- model/message.model.js
+++ model/message.model.js
@@ -7,7 +7,7 @@ const messageSchema = new mongoose.Schema({
     },
     chat:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"chat"
+        ref:"Chat"
     },
     content:{
         type:String,
