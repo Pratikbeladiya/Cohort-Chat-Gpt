28336c897a35e91b3d1253a54714835a8d7df3b9
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-09-13 16:26:00 +0530

    feat: modified the server file #20

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const app = require("./app");
 
+//call the connect Database function that actual connect server with database 
 const ConnectDb = require("./db/db");
 ConnectDb();
+
 const initSocketServer = require("./sockets/socket.server");
 const httpServer = require("http").createServer(app);
 
